afd551372e3552968262ccefa0dee528f3c11442
yekhong <sknye1004@gmail.com>
2026-07-26 16:24:26 +0900

    Public 전 키값 비노출 설정
    public 설정 전 키값 환경변수로 변경

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://ebgnvedocneqkibgnpee.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImViZ252ZWRvY25lcWtpYmducGVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ4NjA2MjIsImV4cCI6MjEwMDQzNjYyMn0.YfqPj0LgI6lrvopSv4IcLbQM5YxdfxG_jSZ4XaU1gsU';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
